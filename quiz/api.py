a06ebb196e26524bfa8e452d9d73689020567733
Daniel <86085164+msck1@users.noreply.github.com>
2024-11-15 18:22:28 -0300

    Consertando chamada api gemini

--- quiz/api.py
+++ quiz/api.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,30 +18,21 @@ def gerar_questao(area, materia, assunto):
         dict: Dicionário contendo a resposta da API ou informações sobre o erro.
     """
 
-    chave_api = os.getenv("GEMINI_API_KEY")
-    if not chave_api:
-        return {"error": "Chave da API não encontrada. Verifique o arquivo .env."}
-
-    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDKilv1qBzn8ewXSziCaoWzUOM_aQWVVhU"
-    headers = {}
-
     # **Adapte o payload de acordo com a documentação da API**
-    payload = {
-        "prompt": f"Gere uma questão sobre {assunto} em {materia}, na área de {area}.",
-        "temperature": 0.7  # Ajuste a temperatura para controlar a criatividade da resposta
-    }
-
     try:
-        response = requests.post(url, json=payload, headers=headers)
-        response.raise_for_status()  # Lança uma exceção para status codes diferentes de 200
+        genai.configure(api_key="AIzaSyAA4jvpnCviU9iX8fLlu9YIK4XKN8PS08M")
+        prompt = f"Gere uma questão sobre {assunto} em {materia}, na área de {area}."
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content(prompt)
+        print(response.text)
 
-        # Extrair a resposta da API
-        resposta_ia = response.json()
-        # Processar a resposta da IA (extrair a questão e as alternativas)
-        # ...
+    except:
+        print("Algum erro ocorreu!")
 
-        return resposta_ia
-    except requests.exceptions.RequestException as e:
-        return {"error": f"Erro ao conectar com a API: {e}"}
-    except json.JSONDecodeError as e:
-        return {"error": f"Erro ao processar a resposta JSON: {e}"}
+    
+# exemplo de codigo da doc que funciona    
+# model = genai.GenerativeModel("gemini-1.5-flash")
+# response = model.generate_content("Write a story about a magic backpack.")
+# print(response.text)
+
+gerar_questao("Subtração","Matematica", "Algebra")
